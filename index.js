938f0ee9c3f3f2247e7b7fc279cb94c6edc60ddd
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-02-10 17:53:51 -0500

    added all questions to index.js and worked on shapes and started 1st test

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 const inquirer = require ('inquirer'); 
-console.log(inquirer); 
 
-const jest = require ('jest'); 
-console.log(jest); 
+const fs = require ('fs'); 
+
+
+// create terminal questions to generate logo 
+
+inquirer 
+    .prompt([
+        {
+            type: 'input', 
+            message: '*MAXIMUM 3 LETTERS* Enter 3 letters of text for your logo.',
+            name: 'logo', 
+ 
+        }, 
+        {
+            type: 'input', 
+            message: 'Enter a color keyword or a hexadecimal number for your TEXT color.', 
+            name: 'textcolor', 
+
+        }, 
+        {
+            type: 'list', 
+            message: 'Choose a shape for your logo.', 
+            name: 'shape', 
+            choices: ['Circle', 'Triangle', 'Square']
+        },
+        {
+            type: 'input', 
+            message: 'Enter a color keyword or a hexadecimal number for your SHAPE COLOR.', 
+            name: 'shapecolor', 
+
+        },
+    ]);
+
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) => 
+        err ? console.log(err): console.log("Generated logo.svg"))
+}
+
+function init() {
+    inquirer.prompt(questions)
+    .then(response =>{
+        let readText = utils(response)
+        writeToFile('logo.svg', readText)
+    }) 
+}
+
+init();
