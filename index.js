4de98af72004581f2f36ac8ae077d19598454416
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-02-11 15:57:58 -0500

    shapeChoice to determine logo shape and working on tests

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const questions = [
    
         {
             type: 'input', 
-            message: '*MAXIMUM 3 LETTERS* Enter 3 letters of text for your logo.',// add failure check for max 3 characters
+            message: '*MAXIMUM 3 LETTERS* Enter 3 letters of text for your logo.',// add failure check for max 3 characters & no text = DONE!
             name: 'logo', 
  
         }, 
@@ -55,36 +55,71 @@ const questions = [
 
 /// trying to create function to determine the selection of square, circle or triangle...... 
 /// TRYING.... 
-function shapeChoice(shape){
-    if (shape === circle){
-        console.log(response); 
-        const circle = new Circle(response.logo, response.textcolor, response.shapecolor);
-        writeToFile('logo.svg', circle.render())
-    }
-    else if (shape === triangle){
-        console.log(response); 
-        const triangle = new Triangle(response.logo, response.textcolor, response.shapecolor);
-        writeToFile('logo.svg', triangle.render())
-    }
-    else if (shape === square){
-        console.log(response); 
-        const square = new Square(response.logo, response.textcolor, response.shapecolor);
-        writeToFile('logo.svg', square.render())
+// function shapeChoice(shape){
+//     if (shape === circle){
+//         console.log(response); 
+//         const circle = new Circle(response.logo, response.textcolor, response.shapecolor);
+//         writeToFile('logo.svg', circle.render())
+//     }
+//     else if (shape === triangle){
+//         console.log(response); 
+//         const triangle = new Triangle(response.logo, response.textcolor, response.shapecolor);
+//         writeToFile('logo.svg', triangle.render())
+//     }
+//     else if (shape === square){
+//         console.log(response); 
+//         const square = new Square(response.logo, response.textcolor, response.shapecolor);
+//         writeToFile('logo.svg', square.render())
+//     }
+// }
+
+// shapeChoice to determine logo shape 
+function shapeChoice(response) {
+    // The switch statement within the function defines the shape chosen by the user
+    // it creates an object for that shape with the parameters specified (e.g. logo, text color, shape color).
+    switch (response.shape) {
+        case 'circle':
+            const circle = new Circle(response.logo, response.textcolor, response.shapecolor);
+            writeToFile('logo.svg', circle.render()) // the object's render() method is triggered, writing out the logo as a SVG file.
+            break; 
+
+        case 'triangle':
+            const triangle = new Triangle(response.logo, response.textcolor, response.shapecolor);
+            writeToFile('logo.svg', triangle.render())
+            break;
+
+        case 'square':
+            const square = new Square(response.logo, response.textcolor, response.shapecolor);
+            writeToFile('logo.svg', square.render())
+            break;// tells computer to stop 
+
+        // default:
+        //     console.error('Invalid shape choice');
+        //     break;
     }
 }
 
 // used to create logo based on user input 
 function init() {
-    // inquirer.prompt method to ask user set of questions ^ 
+     // inquirer.prompt method to ask user set of questions ^ 
     inquirer.prompt(questions)
-    .then(response =>{ // user responses stored in response variable
-        console.log(response);
-        // created circle object with logo,textcolor,shapecolor from response variable
-        const circle = new Circle(response.logo, response.textcolor, response.shapecolor);
-        // writeToFile to create svg file. circle.render to make logo
-        writeToFile('logo.svg', circle.render())
-    }) 
+        .then((response) => { // user responses stored in response variable
+            shapeChoice(response); 
+        })
 }
+
+// used to create logo based on user input 
+// function init() {
+//     // inquirer.prompt method to ask user set of questions ^ 
+//     inquirer.prompt(questions)
+//     .then(response =>{ // user responses stored in response variable
+//         console.log(response);
+//         // created circle object with logo,textcolor,shapecolor from response variable
+//         const circle = new Circle(response.logo, response.textcolor, response.shapecolor);
+//         // writeToFile to create svg file. circle.render to make logo
+//         writeToFile('logo.svg', circle.render())
+//     }) 
+// }
 
 
 init();
