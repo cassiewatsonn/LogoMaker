d7a33409cb30e2889e79cbd69bef7c6c7689cffb
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-02-14 08:56:26 -0500

    working on resolving test issue

--- index.js
+++ index.js
@@ -19,13 +19,15 @@ const questions = [
         {
             type: 'input', 
             message: '*MAXIMUM 3 LETTERS* Enter 3 letters of text for your logo.',// add failure check for max 3 characters & no text = DONE!
-            name: 'logo', 
+            name: 'logo',
+            validate: logoLength, 
  
         }, 
         {
             type: 'input', 
             message: 'Enter a color keyword or a hexadecimal number for your TEXT color.', 
             name: 'textcolor', // failure check????
+            validate: isValidHexaCode,
 
         }, 
         {
@@ -38,9 +40,42 @@ const questions = [
             type: 'input', 
             message: 'Enter a color keyword or a hexadecimal number for your SHAPE COLOR.', 
             name: 'shapecolor', /// failure check???? 
+            validate: isValidHexaCode,
 
         },
     ];
+
+    //Function to test for logo length, must neither be null or over 3 characters     
+function logoLength(response) {
+if (!response) {
+    return "Please enter a valid response";
+    }
+if (response.length > 3) {
+    return "The logo must be between 1 to 3 characters";
+}
+return true;
+}
+
+
+//Array of the current valid CSS3 colour names to be used in function isValidHexaCode
+var colorList = ['aquamarine','azure','beige','bisque','black','blanchedalmond','blue','blueviolet','brown','burlywood','cadetblue','chartreuse','chocolate','coral','cornflowerblue','cornsilk','crimson','cyan','darkblue','darkcyan','darkgoldenrod','darkgray','darkgreen','darkkhaki','darkmagenta','darkolivegreen','darkorange','darkorchid','darkred','darksalmon','darkseagreen','darkslateblue','darkslategray','darkturquoise','darkviolet','deeppink','deepskyblue','dimgray','dodgerblue','firebrick','floralwhite','forestgreen','fuchsia','gainsboro','ghostwhite','gold','goldenrod','gray','green','greenyellow','honeydew','hotpink','indianred','indigo','ivory','khaki','lavender','lavenderblush','lawngreen','lemonchiffon','lightblue','lightcoral','lightcyan','lightgoldenrodyellow','lightgrey','lightgreen','lightpink','lightsalmon','lightseagreen','lightskyblue','lightslategray','lightsteelblue','lightyellow','lime','limegreen','linen','magenta','maroon','mediumaquamarine','mediumblue','mediumorchid','mediumpurple','mediumseagreen','mediumslateblue','mediumspringgreen','mediumturquoise','mediumvioletred','midnightblue','mintcream','mistyrose','moccasin','navajowhite','navy','oldlace','olive','olivedrab','orange','orangered','orchid','palegoldenrod','palegreen','paleturquoise','palevioletred','papayawhip','peachpuff','peru','pink','plum','powderblue','purple','red','rosybrown','royalblue','saddlebrown','salmon','sandybrown','seagreen','seashell','sienna','silver','skyblue','slateblue','slategray','snow','springgreen','steelblue','tan','teal','thistle', 'tomato','turquoise','violet','wheat','white','whitesmoke','yellow','yellowgreen', ];  
+
+//Function to test for colours for both textcolor & shapecolor 
+function isValidHexaCode(response) {
+    // Regex to check for valid hexadecimalColor_code 
+    let regex = new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
+    // If response is null, prompts user to enter a colour value
+    if (!response) {
+        return "Please enter a valid colour value";
+    }
+    // Returns true if the response matches either the colorList or the regex, else prompts the user to enter a valid colour
+    if (colorList.includes(response) || regex.test(response) ) {
+        return true;
+    }
+    else {
+        return "Please enter a valid colour value ";
+    }
+}
 
     // used to write given data to a file with logo.svg name
     // fileName is name of file i want to write to 
@@ -105,6 +140,7 @@ function init() {
      // inquirer.prompt method to ask user set of questions ^ 
     inquirer.prompt(questions)
         .then((response) => { // user responses stored in response variable
+            isValidHexaCode(response);
             shapeChoice(response); 
         })
 }
